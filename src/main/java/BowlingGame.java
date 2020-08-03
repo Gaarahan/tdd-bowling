6d07331c6d8d914605a008a921a85c5ddf749dd4
gaarahan <gaarahan@foxmail.com>
2020-08-03 20:36:08 +0800

    [Gaarahan] Could resolve none-hit situation

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,13 @@
+/**
+ * @author gaarahan
+ */
+public class BowlingGame {
+  public int score;
+
+  public BowlingGame() {
+    this.score = 0;
+  }
+
+  public void throwBall(int frame, int i) {
+  }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BowlingGameTest {
+  @Test
+  void should_got_score_0_when_non_pin_is_knockdown_in_one_line () {
+    BowlingGame game = new BowlingGame();
+    for (int frame = 1; frame <= 10; frame ++) {
+      game.throwBall(frame, 0);
+    }
+    assertEquals(0, game.score);
+  }
 }
