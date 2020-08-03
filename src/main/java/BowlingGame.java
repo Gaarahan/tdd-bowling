98dda61022f58290642e72bb8e42cb88ec382831
gaarahan <gaarahan@foxmail.com>
2020-08-03 23:16:47 +0800

    [Gaarahan] all ac is passed

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,13 +1,114 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
 /**
  * @author gaarahan
  */
 public class BowlingGame {
-  public int score;
+  private int frame;
+  private final ArrayList<ArrayList<Integer>> scoreList;
 
   public BowlingGame() {
-    this.score = 0;
+    this.frame = 0;
+    this.scoreList = new ArrayList<>();
+    for (int framePoint = 0; framePoint < 10; framePoint++) {
+      this.scoreList.add(new ArrayList<>());
+    }
   }
 
-  public void throwBall(int frame, int i) {
+  public void throwBall(int curScore) {
+    // first nine frame
+    if (this.frame < 9) {
+      //first throw
+      if (scoreList.get(this.frame).size() == 0) {
+        // strike
+        if (curScore == 10) {
+          scoreList.get(this.frame).add(curScore);
+          // no second throw chance
+          this.frame++;
+        } else {
+          scoreList.get(this.frame).add(curScore);
+        }
+      } else {
+        // second chance
+        scoreList.get(this.frame).add(curScore);
+        this.frame++;
+      }
+    } else if (this.frame == 9) {
+      // first throw
+      if (this.scoreList.get(this.frame).size() == 0) {
+        scoreList.get(this.frame).add(curScore);
+      }
+      // second throw
+      else if (this.scoreList.get(this.frame).size() == 1) {
+        if (this.scoreList.get(this.frame).get(0) == 10) {
+          this.scoreList.get(this.frame).add(curScore);
+        } else if (this.scoreList.get(this.frame).get(0) + curScore == 10) {
+          this.scoreList.get(this.frame).add(curScore);
+        } else {
+          this.scoreList.get(this.frame).add(curScore);
+          this.frame++;
+        }
+      }
+      // third throw
+      else if (this.scoreList.get(this.frame).size() == 2) {
+        this.scoreList.get(this.frame).add(curScore);
+        this.frame++;
+      }
+    } else {
+      System.out.println("You throw 10 frame,this bowling game is over.");
+    }
+
+  }
+
+  public int getScore() {
+    int score = 0;
+     // count first nine throw
+    for (int framePoint = 0; framePoint < 9; framePoint++) {
+      ArrayList<Integer> curFrame = this.scoreList.get(framePoint);
+
+      int firstScore = curFrame.get(0);
+      if (firstScore == 10) {
+        score += firstScore + this.getNextTwoThrowScore(framePoint);
+      } else {
+        int secondScore = curFrame.get(1);
+        if (firstScore + secondScore == 10) {
+          score += 10 + this.scoreList.get(framePoint + 1).get(0);
+        } else {
+          score += firstScore + secondScore;
+        }
+      }
+    }
+
+    // count the tenth throw
+    ArrayList<Integer> theTenthThrow = this.scoreList.get(9);
+    int firstScore = theTenthThrow.get(0);
+    int secondScore = theTenthThrow.get(1);
+    if (firstScore == 10) {
+      score += firstScore + secondScore + theTenthThrow.get(2);
+    } else if (firstScore + secondScore == 10) {
+      score += 10 + theTenthThrow.get(2);
+    } else {
+      score += firstScore + secondScore;
+    }
+
+    return score;
+  }
+
+  private int getNextTwoThrowScore(int curFramePoint) {
+    ArrayList<Integer> nextFrame = this.scoreList.get(curFramePoint + 1);
+    int nextScore = nextFrame.get(0);
+    int nextNextScore;
+    if (nextScore == 10) {
+      // next throw is the tenth throw
+      if (this.frame + 1 == 9) {
+        nextNextScore = nextFrame.get(1);
+      } else {
+        nextNextScore = this.scoreList.get(curFramePoint + 2).get(0);
+      }
+    } else {
+      nextNextScore = nextFrame.get(1);
+    }
+    return nextScore + nextNextScore;
   }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -6,8 +6,99 @@ public class BowlingGameTest {
   void should_got_score_0_when_non_pin_is_knockdown_in_one_line () {
     BowlingGame game = new BowlingGame();
     for (int frame = 1; frame <= 10; frame ++) {
-      game.throwBall(frame, 0);
+      game.throwBall(0);
     }
-    assertEquals(0, game.score);
+    assertEquals(0, game.getScore());
+  }
+
+  @Test
+  void should_got_correct_score_when_first_nine_throw_is_not_strike_or_spare () {
+    BowlingGame game = new BowlingGame();
+    for (int frame = 1; frame < 10; frame ++) {
+      game.throwBall(5);
+      game.throwBall(2);
+    }
+
+    // the tenth throw
+    game.throwBall(0);
+    game.throwBall(0);
+
+    assertEquals(63, game.getScore());
+  }
+
+  @Test
+  void should_got_correct_score_when_first_nine_is_strike () {
+    BowlingGame game = new BowlingGame();
+    for (int frame = 1; frame < 10; frame ++) {
+      game.throwBall(10);
+    }
+
+    // the tenth throw
+    game.throwBall(0);
+    game.throwBall(0);
+
+    assertEquals(240, game.getScore());
+  }
+
+  @Test
+  void should_got_correct_score_when_first_nine_is_spare () {
+    BowlingGame game = new BowlingGame();
+    for (int frame = 1; frame < 10; frame ++) {
+      game.throwBall(7);
+      game.throwBall(3);
+    }
+
+    // the tenth throw
+    game.throwBall(0);
+    game.throwBall(0);
+
+    assertEquals(146, game.getScore());
+  }
+
+  @Test
+  void should_got_correct_score_when_the_tenth_strike () {
+    BowlingGame game = new BowlingGame();
+    for (int frame = 1; frame < 10; frame ++) {
+      game.throwBall(0);
+      game.throwBall(0);
+    }
+
+    // the tenth throw
+    game.throwBall(10);
+    game.throwBall(4);
+    game.throwBall(5);
+
+    assertEquals(19, game.getScore());
+  }
+
+  @Test
+  void should_got_correct_score_when_the_tenth_spare () {
+    BowlingGame game = new BowlingGame();
+    for (int frame = 1; frame < 10; frame ++) {
+      game.throwBall(0);
+      game.throwBall(0);
+    }
+
+    // the tenth throw
+    game.throwBall(6);
+    game.throwBall(4);
+    game.throwBall(5);
+
+    assertEquals(15, game.getScore());
+  }
+
+  @Test
+  void should_got_correct_score_when_the_tenth_not_strike_or_spare () {
+    BowlingGame game = new BowlingGame();
+    for (int frame = 1; frame < 10; frame ++) {
+      game.throwBall(0);
+      game.throwBall(0);
+    }
+
+    // the tenth throw
+    game.throwBall(5);
+    game.throwBall(4);
+
+    assertEquals(9, game.getScore());
   }
 }
